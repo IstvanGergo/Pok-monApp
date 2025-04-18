75c93a012686219e0bcdfead9e9d6c401b134a18
IstvanGergo <istvangergo96@gmail.com>
2025-04-18 19:05:21 +0200

    feat: add move getters

---
+++ app/src/main/java/com/example/pokemonapp/data/Move.java
@@ -0,0 +1,20 @@
+package com.example.pokemonapp.data;
+
+import com.example.pokemonapp.model.Move.AllMoves;
+import com.example.pokemonapp.model.Move.MoveDetails;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import retrofit2.http.Url;
+
+public interface Move {
+    @GET("move/?limit=100000&offset=0.")
+    Call<AllMoves> getAllMoves();
+    @GET("move/")
+    Call<AllMoves> getMoves(@Query("offset") int offset, @Query("limit") int limit);
+    @GET
+    Call<MoveDetails> getMoveDetail(@Url String url);
+    @GET("move/")
+    Call<Move> getSpecificMove(@Query("") String moveName);
+}
